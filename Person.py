c407471c57fbd786776df57ab47048960e34d1dd
MohammedAbuzaid13 <mohammedzaid772@gmail.com>
2024-03-21 22:14:24 -0800

    the firebase database
    click on the database url and sigh in to our shared account to chick on the data base
    when you run the code it supposed to add the date in real time

--- Person.py
+++ Person.py
@@ -1,50 +1,68 @@
 class Person:
 
     def __init__(self, ID, F_Name, L_Name, Email, phone_number, username, password):
-        self._ID = ID
-        self.F_Name = F_Name
-        self.L_Name = L_Name
-        self.Email = Email
-        self.phone_number = phone_number
-        self.username = username
-        self.password = password
+        self.__ID = ID
+        self.__F_Name = F_Name
+        self.__L_Name = L_Name
+        self.__Email = Email
+        self.__phone_number = phone_number
+        self.__username = username
+        self.__password = password
 
     def set_ID(self, id):
-        self._ID = id
+        self.__ID = id
 
     def set_name(self, first_name, last_name):
-        self.F_Name = first_name
-        self.L_Name = last_name
+        self.__F_Name = first_name
+        self.__L_Name = last_name
 
     def set_Email(self, Email):
-        self.Email = Email
+        self.__Email = Email
 
     def set_phone_number(self, phone_number):
-        self.phone_number = phone_number
+        self.__phone_number = phone_number
 
     def set_username(self, username):
-        self.username = username
+        self.__username = username
 
     def set_password(self, password):
-        self.password = password
+        self.__password = password
 
     def get_ID(self):
-        return self._ID
+        return self.__ID
 
     def get_name(self):
-        return self.F_Name, self.L_Name
+        return self.__F_Name, self.__L_Name
 
     def get_Email(self):
-        return self.Email
+        return self.__Email
 
     def get_phone_number(self):
-        return self.phone_number
+        return self.__phone_number
 
     def get_username(self):
-        return self.username
+        return self.__username
 
     def get_password(self):
-        return self.password
+        return self.__password
 
 
-# class supplier(Person):
+class supplier(Person):
+    def __init__(self, ID, F_Name, L_Name, Email, phone_number, username, password, supp_ID):
+        super().__init__(ID, F_Name, L_Name, Email, phone_number, username, password, supp_ID)
+    
+    
+class customer(Person):
+    def __init__(self, ID, F_Name, L_Name, Email, phone_number, username, password,cus_ID):
+        super().__init__(ID, F_Name, L_Name, Email, phone_number, username, password,cus_ID)
+        
+
+class items:
+    def __init__(self, item_ID, item_name, Price, description):
+        self.item_ID = item_ID
+        self.item_name = item_name
+        self.Price = Price
+        self.description = description
+        
+    
+    
