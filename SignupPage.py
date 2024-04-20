0b079d780ce7431095268aa279d38b4ec3fcd9ec
Ahmed Kamal Al Karaki <ahm0201429@ju.edu.jo>
2024-04-20 21:40:01 +0300

    Signup Page and database code
    Signup page pushes to the database in the child Users
    threw some db.py code inside SignupPage.py

--- SignupPage.py
+++ SignupPage.py
@@ -1,24 +1,31 @@
 import hashlib
+import os
 
-from PySide6.QtCore import Qt, QSize
+from PySide6.QtCore import QSize, Qt
 from PySide6.QtGui import QColor
-from PySide6.QtWidgets import QApplication, QLabel, QLineEdit, QPushButton, QWidget
+from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QPushButton,
+                               QWidget)
 
+import db
 import LoginPage
-
-USER_FILE = "users.txt"
 
 
 class signup_page(QWidget):
     def __init__(self):
         super().__init__()
-
+        self.__first_name = ""
+        self.__last_name = ""
+        self.__username = ""
+        self.__email = ""
+        self.__password = ""
+        self.__phone_number = ""
+        self.dtbs = db.firebase.database()
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle("GRADUATION PROJECT")
         self.setFixedSize(QSize(1200, 600))
-        
+
         # the label for the Title
         self.label_build_smart = QLabel("BuildSmart", self)
         self.label_build_smart.setGeometry(0, 50, 1200, 100)
@@ -44,10 +51,10 @@ class signup_page(QWidget):
         )  # White
 
         # the textbox for User Name
-        self.last_name_textbox = QLineEdit(self)
-        self.last_name_textbox.setGeometry(450, 300, 300, 30)
-        self.last_name_textbox.setPlaceholderText("User Name")
-        self.last_name_textbox.setStyleSheet(
+        self.username_textbox = QLineEdit(self)
+        self.username_textbox.setGeometry(450, 300, 300, 30)
+        self.username_textbox.setPlaceholderText("User Name")
+        self.username_textbox.setStyleSheet(
             "background-color: rgb(255, 255, 255);"
         )  # White
 
@@ -83,7 +90,6 @@ class signup_page(QWidget):
             "background-color: rgb(255, 255, 255);"
         )  # White
 
-
         # the button for Sign Up
         self.signup_button = QPushButton("Sign Up", self)
         self.signup_button.setGeometry(625, 550, 125, 30)
@@ -92,12 +98,10 @@ class signup_page(QWidget):
         )  # White
         self.signup_button.clicked.connect(self.openLoginPage)
 
-        #back button
+        # back button
         self.back_button = QPushButton("Back", self)
         self.back_button.setGeometry(450, 550, 125, 30)
-        self.back_button.setStyleSheet(
-            "background-color: rgb(255, 255, 255);"
-        )  # White
+        self.back_button.setStyleSheet("background-color: rgb(255, 255, 255);")  # White
         self.back_button.clicked.connect(self.openLoginPage)
 
         # the label for the contact
@@ -109,35 +113,58 @@ class signup_page(QWidget):
         self.contact_label.setStyleSheet("color: rgb(140, 140, 140);")  # gray
 
         # Set background color using RGB for the window
-        self.setStyleSheet(
-            "background-color: rgb(0, 0, 0);font-weight: bold;"
-        )  # Black
+        self.setStyleSheet("background-color: rgb(0, 0, 0);font-weight: bold;")  # Black
 
         self.show()
-# a function to hash the password **********************************************************
-    # def hash_password(password):
-    #     # """Hash the password ."""
-    #     return hashlib.sha3_512(password.encode()).hexdigest()
 
-# a function to save the inputs *******************************************************************************
-    # def sign_up():
-    #     # """Sign up a new user."""
-    #     F_Name =
-    #     L_Name =
-    #     Email = 
-    #     phone_number = 
-    #     username =
+    def handle_signup_click(self):
+        # Get user input from text boxes
+        self.__first_name = self.first_name_textbox.text()
+        self.__last_name = self.last_name_textbox.text()
+        self.__password = self.password_textbox.text()
+        self.__phone_number = self.phone_number_textbox.text()
+        self.__username = self.username_textbox.text()
+        self.__email = self.email_textbox.text()
 
-# The commented code below is commented because at the moment it doesnt work ********************************************************
+    def push_data_to_database(self):
+        self.handle_signup_click()
+        user_info = {
+            "UserName": self.__username,
+            "FirstName": self.__first_name,
+            "LastName": self.__last_name,
+            "PhoneNumber": self.__phone_number,
+            "Email": self.__email,
+            "Password": self.__password,
+        }
 
-        # if user_exists(username):
-        #     print("Username already exists. Try a different one.")
-        #     return
-    
+        self.dtbs.child("Users").push(user_info)
+
     def openLoginPage(self):
+        self.push_data_to_database()
+
         self.loginPage = LoginPage.login_page()
         self.loginPage.show()
         self.close()
 
 
 # checking with isort
+
+# a function to hash the password **********************************************************
+# def hash_password(password):
+#     # """Hash the password ."""
+#     return hashlib.sha3_512(password.encode()).hexdigest()
+
+# a function to save the inputs *******************************************************************************
+# def sign_up():
+#     # """Sign up a new user."""
+#     F_Name =
+#     L_Name =
+#     Email =
+#     phone_number =
+#     username =
+
+# The commented code below is commented because at the moment it doesnt work ********************************************************
+
+# if user_exists(username):
+#     print("Username already exists. Try a different one.")
+#     return
--- db.py
+++ db.py
@@ -1,24 +1,3 @@
-# import pyrebase
-# from firebase import firebase
-# import firebase
-
-# Config = {
-#     "apiKey": "AIzaSyDq18MssqgcK8FFiGABLBoxh439mEADbn4",
-#     "authDomain": "buildsmart-9c980.firebaseapp.com",
-#     "databaseURL": "https://buildsmart-9c980-default-rtdb.europe-west1.firebasedatabase.app",
-#     "projectId": "buildsmart-9c980",
-#     "storageBucket": "buildsmart-9c980.appspot.com",
-#     "messagingSenderId": "874619547397",
-#     "appId": "1:874619547397:web:e4c18ea945419d4c91916c",
-#     "measurementId": "G-2YER9KMH08",
-# }
-# FB = pyrebase.initialize_app(Config)
-
-
-# db = firebase.database()
-# dat = {"name ": "john", "id ": "1"}
-# db.push(dat)
-# db.child("Data").set(dat)
 import pyrebase
 
 Config = {
@@ -33,7 +12,7 @@ Config = {
 }
 firebase = pyrebase.initialize_app(Config)
 
-db = firebase.database()
-dat = {"name ": "john", "id ": "1"}
-db.push(dat)
-db.child("Data").set(dat)
+
+# dat = {"name ": "john", "id ": "1"}
+# db.push(dat)
+# db.child("Data").set(dat)
