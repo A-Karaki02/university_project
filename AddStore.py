6c8fdd580a506ce01ec73ec3988608139fcccdf4
omarmsh02 <omar.mshahed@gmail.com>
2024-05-07 22:33:16 +0300

    add_store

--- AddStore.py
+++ AddStore.py
@@ -1,7 +1,7 @@
 import sys
 
 from PySide6.QtCore import QSize, Qt
-from PySide6.QtWidgets import (QApplication, QComboBox, QGridLayout, QLabel,
+from PySide6.QtWidgets import (QApplication, QComboBox, QGridLayout, QLabel,QSpinBox,
                                QLineEdit, QPushButton, QVBoxLayout, QWidget)
 
 import Stores
@@ -16,22 +16,27 @@ class add_store(QWidget):
 
     def initUI(self):
         self.setWindowTitle("Add a new Store")
-        self.setFixedSize(400, 150)
+        self.setFixedSize(400, 250)
 
         layout = QVBoxLayout(self)
 
-        store_name_label = QLabel("Store Name:")
-        self.store_name_edit = QLineEdit()
-        layout.addWidget(store_name_label)
-        layout.addWidget(self.store_name_edit)
+        Item_name_label = QLabel("Item Name:")
+        self.Item_name_edit = QLineEdit()
+        layout.addWidget(Item_name_label)
+        layout.addWidget(self.Item_name_edit)
 
-        store_type_label = QLabel("Store Type:")
-        self.store_type_combo = QComboBox()
-        self.store_type_combo.addItems(["Iron and Cement", "Electricals"])
-        layout.addWidget(store_type_label)
-        layout.addWidget(self.store_type_combo)
+        Item_type_label = QLabel("Item Type:")
+        self.Item_type_combo = QComboBox()
+        self.Item_type_combo.addItems(["Iron and Cement", "Electricals"])
+        layout.addWidget(Item_type_label)
+        layout.addWidget(self.Item_type_combo)
 
-        add_button = QPushButton("Add Store")
+        self.Price_label = QLabel("Price:")
+        self.Price_edit = QLineEdit()  
+        layout.addWidget(self.Price_label)
+        layout.addWidget(self.Price_edit)   
+
+        add_button = QPushButton("Add Item")
         add_button.clicked.connect(self.handle_add_item)
         layout.addWidget(add_button)
 
@@ -42,11 +47,17 @@ class add_store(QWidget):
             store_name = user.get_store_name()
         else:
             store_name = user.get_username()
-        store_type = self.store_type_combo.currentText()
-        # self.store_added.emit(store_name, store_type)
-        self.items = {"storeName": store_name, "storeType": store_type}
+        item_name = self.Item_name_edit.text()
+        item_type = self.Item_type_combo.currentText()
+        price = self.Price_edit.text()
+        self.items = {"storeName": store_name, "itemName": item_name,"itemType": item_type, "price": price}
 
     def handle_add_item(self):
         self.add_store()
+
         user.add_item(self.items)
         self.close()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = add_store()
+    sys.exit(app.exec())
